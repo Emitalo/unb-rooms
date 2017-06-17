4e93979cefc8212d53bee33933bb48315f2cd9a5
Emilie Morais <emilie.mo@hotmail.com>
2017-06-17 20:49:19 -0300

    get descendants

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   
   def index
   	@room = Room.new
+  	@room_types = Room.get_types
   end
 
 
--- app/models/room.rb
+++ app/models/room.rb
@@ -6,6 +6,38 @@ class Room
   field :capacity, type: Integer
   field :nickname, type: String
   field :identifier, type: String
+
+  def self.get_types
+    rooms = self.descendants.map(&:name).sort
+    print(rooms)
+    types = []
+    rooms.each do |room|
+      type = {}
+      case room
+        when "RegularClassRoom"
+          type[room] = "Sala de Aula"
+
+        when "SmartRoom"
+          type[room] = "Sala Inteligente"
+        
+        when "RegularConferenceRoom"
+          type[room] = "Sala de Conferência comum"
+
+        when "VideoConferenceRoom"
+          type[room] = "Sala de Vídeo Conferência"
+
+        when "Lab"
+          type[room] = "Laboratório"
+
+      end
+      if(!type.empty?)
+        types.push(type)
+      end
+    end
+
+    return types
+  end
+
 end
 
 class ClassRoom < Room
