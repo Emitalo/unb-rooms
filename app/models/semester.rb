ade580328bec287e35f1b9aa349efaf1dbdf24ac
Italo Paiva <italosensei@hotmail.com>
2017-06-18 18:40:01 -0300

    Identifying current semester

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -4,4 +4,9 @@ class Semester
   field :start_date, type: Date
   field :end_date, type: Date
   field :desc, type: String
+  field :current, type: Boolean
+
+  def self.current_semester
+    find_by current: true
+  end
 end
